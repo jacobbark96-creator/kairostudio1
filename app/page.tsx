95a1e57d91342c8d3e1bae4205dea50822b41d2b
Jake <bjake0192@gmail.com>
2026-03-31 23:42:47 +0700

    Remove legacy PageClientWrapper forcing an old mobile design override

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
 import HomePage from '../src/page-components/HomePage';
-import MobileHome from '../src/mobile/pages/MobileHome';
-import PageClientWrapper from '../src/components/PageClientWrapper';
 
 export default function Page() {
   return (
-    <PageClientWrapper 
-      desktop={<HomePage />} 
-      mobile={<MobileHome />} 
-    />
+    <HomePage />
   );
 }
