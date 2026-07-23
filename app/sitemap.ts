2df141351338302629a0f204af47ae81a053d364
Jake Bedwell <jake@Jakes-MacBook-Pro.local>
2026-07-23 23:37:07 +0700

    fix: disable static caching for sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next';
 import { createClient } from '@supabase/supabase-js';
 
+export const dynamic = 'force-dynamic';
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = 'https://kairostudio.co.uk';
   
